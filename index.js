ec4c336378353906cd1397a912d374c1ff5aaf2b
siriusmart <siriusmart@tuta.io>
2025-06-30 14:13:07 +0100

    bump

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 let fs = module.require("/modules/fs");
 
 function requireHelper(callerModule, path, mode) {
-    if (!path.startsWith('.')) {
-        return callerModule.require(`/modules/${path}`);
-    }
-
     if (path.endsWith(".json")) {
         if (!path.startsWith('/')) {
             path = `${java.lang.String.join('/', Array.from(callerModule.path).slice(0, callerModule.path.length - 1))}/${path}`;
+        } else {
+            path = path.replace(/^\/+/, '');
         }
 
         let content = fs.readFileSync(path, "utf8");
 
         return JSON.parse(content);
+    }
+
+    if (!path.startsWith('.')) {
+        if (mode) {
+            return callerModule.require(`/modules/${path}`, mode);
+        } else {
+            return callerModule.require(`/modules/${path}`);
+        }
     }
 
     if (mode) {
